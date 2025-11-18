1e2046d224dad8de6826afa494abf131268a5071
Galina Z <g.n.zamyatina@gmail.com>
2025-11-19 00:51:50 +0300

    Add files via upload
    added a pic, need to replace 3 other files

---
+++ js.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+
+/*На кнопке обработчик, который ждёт, чтобы по клику запустить команду*/
+button.addEventListener('click', function() {
+	alert('你好！你吃了吗？')
+})
